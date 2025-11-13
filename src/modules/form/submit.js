a44705f561253aeaed83cf2a62d4bd998403c934
JuliaMoriC <juliamoridev@gmail.com>
2025-11-13 19:47:38 -0300

    inital commit - upload full project

---
+++ src/modules/form/submit.js
@@ -0,0 +1,52 @@
+import dayjs from "dayjs"
+
+import { scheduleNew } from "../../services/schedules-new.js"
+import { schedulesDay } from "../schedules/load.js"
+
+const form = document.querySelector("form")
+const selectedDate = document.getElementById("date")
+const clientName = document.getElementById("client")
+const inputToday = dayjs(new Date()).format("YYYY-MM-DD")
+
+selectedDate.value = inputToday
+selectedDate.min = inputToday
+
+form.onsubmit = async (event) => {
+    event.preventDefault()
+
+    try {
+        const name = clientName.value.trim()
+        
+        if(!name){
+            return alert("Por favor, insira seu nome.")
+        }
+
+        const hourSelected = document.querySelector(".hour-selected")
+        
+        if(!hourSelected){
+            return alert("Selecione a hora desejada para o agendamento.")
+        }
+
+        const [hour] = hourSelected.innerText.split(":")
+        
+        const when = dayjs(selectedDate.value).add(hour, "hour")
+        
+        const id = new Date().getTime()
+
+        await scheduleNew({
+            id,
+            name,
+            when,
+        })
+
+        await schedulesDay()
+
+        clientName.value = ""
+
+    } catch (error) {
+        alert("Não foi possível realizar o agendamento.")
+        console.log(error)
+    }
+
+   
+}
